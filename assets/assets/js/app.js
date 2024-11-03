61dd7babc286144b8be3eed29d629360a189d22c
Maycon Wendell <164102300+mayconwendell@users.noreply.github.com>
2024-11-03 19:10:04 -0300

    att

---
+++ assets/assets/js/app.js
@@ -0,0 +1,29 @@
+
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const mobileMenu = document.getElementById('mobile-menu');
+
+
+hamburgerIcon.addEventListener('click', () => {
+
+  hamburgerIcon.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+});
+
+var produtoImg = document.getElementById("produtoImg");
+var produtoMiniatura = document.getElementsByClassName("produtoMiniatura");
+
+produtoMiniatura[0].onclick = function (){
+  produtoImg.src = produtoMiniatura[0].src;
+}
+
+produtoMiniatura[1].onclick = function (){
+  produtoImg.src = produtoMiniatura[1].src;
+}
+
+produtoMiniatura[2].onclick = function (){
+  produtoImg.src = produtoMiniatura[2].src;
+}
+
+produtoMiniatura[3].onclick = function (){
+  produtoImg.src = produtoMiniatura[3].src;
+}
